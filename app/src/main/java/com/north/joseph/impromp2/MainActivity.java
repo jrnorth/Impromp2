f31972b4bc36c9aa7605d95b50ba9c1e9b2f585e
Joe North <joernorth@gmail.com>
2014-11-23 15:18:02 -0700

    Added Parse library

--- app/src/main/java/com/north/joseph/impromp2/MainActivity.java
+++ app/src/main/java/com/north/joseph/impromp2/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "", "");
         setContentView(R.layout.activity_main);
     }
 
