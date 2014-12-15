2efb148c6159ae3b90ff610f362a44bc3587da0a
Joseph North <joernorth@gmail.com>
2014-12-15 14:11:57 -0600

    Changed event list adapter to ViewHolder pattern

--- app/src/main/java/com/north/joseph/impromp2/fragments/EventListAdapter.java
+++ app/src/main/java/com/north/joseph/impromp2/fragments/EventListAdapter.java
@@ -1,13 +1,18 @@
 package com.north.joseph.impromp2.fragments;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.north.joseph.impromp2.R;
 import com.north.joseph.impromp2.items.Event;
-import com.north.joseph.impromp2.views.EventView;
+import com.north.joseph.impromp2.views.ViewHolder;
 
+import org.json.JSONException;
+
+import java.text.ParseException;
 import java.util.List;
 
 /**
@@ -15,16 +20,41 @@ import java.util.List;
  */
 public class EventListAdapter extends ArrayAdapter<Event> {
     public EventListAdapter(Context c, List<Event> events) {
-        super(c, 0, events);
+        super(c, R.layout.event_view_children, R.id.event_nameTextView, events);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        EventView eventView = (EventView) convertView;
-        if (eventView == null)
-            eventView = EventView.inflate(parent);
+        View row = super.getView(position, convertView, parent);
+        ViewHolder holder = (ViewHolder) row.getTag();
 
-        eventView.setEvent(getItem(position));
-        return eventView;
+        if (holder == null) {
+            holder = new ViewHolder(row);
+            row.setTag(holder);
+        }
+
+        Event event = getItem(position);
+
+        final String eventName = event.getName();
+
+        try {
+            final String startTime = event.getFormattedStartTime();
+            holder.mTimeTextView.setText(startTime);
+        } catch (ParseException e) {
+            Log.d("ParseException", e.getMessage());
+            holder.mTimeTextView.setText(event.getStartTime());
+        }
+
+        try {
+            final String venueName = event.getVenueName();
+            final String city = event.getCity();
+            holder.mLocationTextView.setText(venueName + ", " + city);
+        } catch (JSONException e) {
+            holder.mLocationTextView.setText("Unknown");
+        }
+
+        holder.mNameTextView.setText(eventName);
+
+        return row;
     }
 }
